d6fd2c2b15732c238dec9d758fa348ad7350a9ff
Ajiva-D <davidenochaji@gmail.com>
2019-04-01 12:54:29 +0100

    more js

--- scripts/list.js
+++ scripts/list.js
@@ -12,9 +12,39 @@ $(function () {
 		}
 	];
 	var addnewBundle = function () {
-		for (let i = 0; i < bundleList.length; i++){
-			$(".bundleTitle").text(bundleList[i].task);
-			$(".bundleDate")
-		}
+		var container = $('.bundleCon');
+		$.each(bundleList,function(i,bundleList){
+    
+			// Let's create the DOM
+			var div = $('<div>'),
+					para = $('<p>'),
+				icon = $('<i>'),
+				img = $('<img>');
+			
+			var listDiv = div.attr('class', 'list-card'),
+				cardDiv = div.attr('class', 'card'),
+				title = para.text(bundleList.task),
+				details = div.attr('class', 'details-row')
+			dateDiv = div.attr('id', 'due-date')
+			datePara = para.attr('class', 'date-input')
+				comment = ;
+			
+			
+			container.append(listDiv);
+			listDiv.append(cardDiv);
+
+			// Add content to the DOM
+			// link.attr('href',bundleList.docSrc)
+			// .text(bundleList.docTitle)
+			// .appendTo(title);
+			
+			// info.text(bundleList.docInfo);
+			
+			// // Append the infos to the item
+			// item.append(title,info);
+			
+			// // Append the item to the container
+			// item.appendTo(container);
+	});
 	}
 });
